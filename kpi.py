7d785e61c4d7b88a72a55e060b9601f805570070
albertohco <acordeiro@3anet.com.br>
2024-02-20 09:20:29 -0300

    Alteração constante kpi.py

--- kpi.py
+++ kpi.py
@@ -1,5 +1,7 @@
+CONSTANTE_BONUS = 1000
+
 nome = input("Digite seu nome: ")
 salario = float(input("Digite seu salario: "))
 bonus = float(input("Digite seu bonus: "))
-kpi = 1000 + salario * bonus
+kpi = CONSTANTE_BONUS + salario * bonus
 print(f"Olá {nome}, o seu bônus foi de {kpi}")
